6af6c43fe2b500e128292a3567676513bc7b835f
Vlad Paulechka <vlad9.ru@gmail.com>
2023-09-29 23:06:17 +0300

    update

--- handlers/users/mainForm.py
+++ handlers/users/mainForm.py
@@ -197,6 +197,27 @@ async def registration_start1(message: types.Message):
     await asyncio.gather(*tasks2, return_exceptions=True)  # Отправка всем админам сразу
 
 
+@dp.message_handler(IsAdmin(), commands=["71"])  # 13 августа в 10.00
+async def registration_start1(message: types.Message):
+    album = MediaGroup()  # /opt/git/webinarTelegramBot/
+    photo1 = InputFile(path_or_bytesio='/opt/git/webinarTelegramBot/1.jpg')
+    photo2 = InputFile(path_or_bytesio='/opt/git/webinarTelegramBot/2.jpg')
+    photo3 = InputFile(path_or_bytesio='/opt/git/webinarTelegramBot/3.jpg')
+
+    tasks2 = []
+    album.attach_photo(photo=photo1)
+    album.attach_photo(photo=photo2)
+    album.attach_photo(photo=photo3)
+
+    users = await CRUDUser.get_all()
+    for user in users:
+        tasks2.append(bot.send_media_group(chat_id=user.user_id,
+                                           media=album
+                                           ))
+
+    await asyncio.gather(*tasks2, return_exceptions=True)  # Отправка всем админам сразу
+
+
 @dp.message_handler(IsAdmin(), commands=["testAudio"])  # 13 августа в 10.00
 async def registration_start1(message: types.Message):
 
